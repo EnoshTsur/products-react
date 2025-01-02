428e4364773d8a5b7d49c91d9cd6bbb06745aba3
EnoshMcCarthy <enoshh12@gmail.com>
2025-01-02 11:34:21 +0200

    all

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,36 +1,44 @@
 import { styled } from "styled-components";
 import { ProductItemData } from "../../hooks/useFetchProducts";
 import Button from "../ui/Button/Button";
+import useCartStore from "../../store/cartStore";
 
 const ProductContainer = styled.div`
-    padding: 1rem;
-    border-radius: 5px;
-    border: 1px solid #d3d3d3;
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    text-align: center;
-    max-width: 20rem;
-    justify-content: space-between;
-    align-items: center;
-`
+  padding: 1rem;
+  border-radius: 5px;
+  border: 1px solid #d3d3d3;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  text-align: center;
+  max-width: 20rem;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const ProductImage = styled.img`
-    width: 5rem;
-    border-radius: 5px;
-`
+  width: 5rem;
+  border-radius: 5px;
+`;
 
 interface ProductItemProps {
-    readonly data: ProductItemData
+  readonly data: ProductItemData;
 }
 
-const ProductItem = ({data: { title, description, price, image }}: ProductItemProps) => (
-    <ProductContainer>
-        <ProductImage src={image} />
-        <h2>{title} {price}$</h2>
-        <p>{description}</p>
-        <Button>Add to cart</Button>
-    </ProductContainer>
-)
+const ProductItem = ({ data }: ProductItemProps) => {
+  const { image, title, price, description } = data;
+  const  { addItem } = useCartStore()
 
-export default ProductItem
+  return (
+    <ProductContainer>
+      <ProductImage src={image} />
+      <h2>
+        {title} {price}$
+      </h2>
+      <p>{description}</p>
+      <Button onClick={() => addItem(data)}>Add to cart</Button>
+    </ProductContainer>
+  );
+};
+
+export default ProductItem;
