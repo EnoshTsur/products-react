8d8c98ab87e9ade5b3bdde4d0aeb82a1faed4dd2
EnoshMcCarthy <enoshh12@gmail.com>
2025-01-02 10:15:41 +0200

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import List from "./components/List/List";
 import Navigation from "./components/Navigation/Navigation";
-import ProductItem from "./components/ProductItem/ProductItem";
-import useFetchProducts from "./hooks/useFetchProducts";
 import AppRouter from "./router/AppRouter";
 
-const App = () => {
-  const { data, error, loading } = useFetchProducts();
-
-  if (error) {
-    return <h1>{error}</h1>;
-  }
-
-  return loading ? (
-    <h1>Loading...</h1>
-  ) : (
-    <>
+const App = () => (
       <AppRouter navigator={<Navigation />} />
-      { data && <List data={data} Item={ProductItem} />}
-    </>
-  );
-};
+)
 
 export default App;
